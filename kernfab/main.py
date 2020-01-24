bb42edc6d041239e2b045e3034d7985fa0247085
hwipl <33433250+hwipl@users.noreply.github.com>
2020-01-24 18:38:28 +0100

    add "build" subcommand to command line arguments
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/main.py
+++ kernfab/main.py
@@ -7,16 +7,41 @@ import argparse
 VERSION = "0.1"
 
 
+def build(_args) -> None:
+    """
+    Build kernel
+    """
+
+    print("Build kernel")
+
+
 def parse_args() -> None:
     """
     Parse command line arguments.
     """
 
-    # if we add more, consider moving it to config or somewhere else
+    # create main parser
     parser = argparse.ArgumentParser(
         description="Run the kernfab linux kernel development tool.")
     parser.add_argument("--version", action="version", version=VERSION)
-    parser.parse_args()
+    parser.set_defaults(func=None)
+
+    # add subcommand parsers
+    subparsers = parser.add_subparsers(title="subcommands",
+                                       description="valid subcommands",
+                                       help="list of subcommands")
+
+    # create the parser for the "build" command
+    parser_build = subparsers.add_parser("build", help="build kernel")
+    parser_build.set_defaults(func=build)
+
+    # parse arguments and call subcommand functions
+    args = parser.parse_args()
+    if args.func:
+        args.func(args)
+    else:
+        # no subcommand, show help
+        parser.print_help()
 
 
 # main entry point
