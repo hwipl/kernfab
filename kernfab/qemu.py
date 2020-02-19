21c296ea278f576a0ad8eb13fd38a07649a30a5c
hwipl <33433250+hwipl@users.noreply.github.com>
2020-02-19 23:01:10 +0100

    add kernfab/qemu.py
    Add initial version of qemu module.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

---
+++ kernfab/qemu.py
@@ -0,0 +1,56 @@
+"""
+Module for creating virtual machine images and
+running virtual machines with qemu
+"""
+
+import invoke
+from fabric import Connection
+
+# name of qemu executable
+QEMU = "qemu-system-x86_64"
+
+
+def _run_cmd(host: str, cmd: str) -> None:
+    """
+    Run a command
+    """
+
+    cmd = f"bash -l -c \"{cmd}\""
+    if host == "":
+        invoke.run(cmd, warn=True)
+    else:
+        conn = Connection(host)
+        conn.run(cmd, warn=True)
+
+
+def create_image() -> None:
+    """
+    Create an vm image
+    """
+
+    image_host = ""
+    file_format = "qcow2"
+    file_name = "qemu.img"
+    file_size = "20G"
+    image_cmd = f"qemu-img create -f {file_format} {file_name} {file_size}"
+    _run_cmd(image_host, image_cmd)
+
+
+def run_vm():
+    """
+    Run a VM
+    """
+
+    host = ""
+    options = "-enable-kvm " \
+        "-m 512 " \
+        "-daemonize " \
+        "-vnc 127.0.0.1:0 " \
+        "-drive discard=unmap,cache=none,file=qemu.img,if=virtio " \
+        "-netdev tap=id=net0,ifname=vmtap0 " \
+        "-device virtio-net-pci,netdev=net0 " \
+        "-object rng-random,filename=/dev/urandom,id=rng0 " \
+        "-device virtio-rng-pci, rng=rng0 " \
+        "-monitor unix:vm0.sock,server,nowait"
+    vm_cmd = f"{QEMU} {options}"
+    _run_cmd(host, vm_cmd)
