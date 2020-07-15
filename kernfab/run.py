4b783c82b8348ec067398ca602420b3148fa24a8
hwipl <33433250+hwipl@users.noreply.github.com>
2020-07-15 20:48:27 +0200

    add run_try() to run.py
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/run.py
+++ kernfab/run.py
@@ -2,8 +2,24 @@
 Module for running commands
 """
 
+import time
+
 import invoke                   # type: ignore
 from fabric import Connection   # type: ignore
+
+
+def _run_cmd(host: str, cmd: str, hide=False) -> invoke.runners.Result:
+    """
+    Helper for running commands
+    """
+
+    cmd = f"bash -l -c \"{cmd}\""
+    if host == "":
+        result = invoke.run(cmd, warn=True, hide=hide)
+    else:
+        conn = Connection(host)
+        result = conn.run(cmd, warn=True, hide=hide)
+    return result
 
 
 def run_cmd(host: str, cmd: str) -> str:
@@ -11,13 +27,23 @@ def run_cmd(host: str, cmd: str) -> str:
     Run a command
     """
 
-    cmd = f"bash -l -c \"{cmd}\""
-    if host == "":
-        result = invoke.run(cmd, warn=True)
-    else:
-        conn = Connection(host)
-        result = conn.run(cmd, warn=True)
+    result = _run_cmd(host, cmd)
     return result.stdout
+
+
+def run_try(host: str, cmd: str, timeout: int) -> str:
+    """
+    Try to run a command multiple times until it is successful for a maximum of
+    timeout seconds
+    """
+
+    start_time = time.time()
+    while time.time() < start_time + timeout:
+        result = _run_cmd(host, cmd, hide=True)
+        if result:
+            return result.stdout
+
+    return ""
 
 
 def run_background(host: str, cmd: str) -> None:
