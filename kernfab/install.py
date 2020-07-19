e78cfc78e34396b0d57bbde3fd8b8e26f470af3d
hwipl <33433250+hwipl@users.noreply.github.com>
2020-07-19 12:45:19 +0200

    add more sub image installing code
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/install.py
+++ kernfab/install.py
@@ -40,6 +40,31 @@ def _chroot_sub_image_install() -> None:
     run.run_cmd(install_host, cmd)
 
 
+def _install_sub_image_kernel() -> None:
+    """
+    Install kernel in sub image
+    """
+
+    mount_dir = "vm-mount"
+    install_host = ""
+    kernel_version = "5.8.0-rc4+"
+
+    # extract kernel archive to mounted vm image
+    kernel_archive = f"linux-{kernel_version}-x86.tar.xz"
+    cmd = f"tar xvfJ {kernel_archive} -C {mount_dir}"
+    run.run_cmd(install_host, cmd)
+
+    # rename/copy installed kernel
+    cmd = f"cp {mount_dir}/boot/vmlinuz-{kernel_version} " \
+        f"{mount_dir}/boot/vmlinuz-linux"
+    run.run_cmd(install_host, cmd)
+
+    # create initramfs for kernel
+    cmd = f"mkinitcpio -g {mount_dir}/boot/initramfs-linux.img " \
+        f"-k {kernel_version} -r {mount_dir}"
+    run.run_cmd(install_host, cmd)
+
+
 def _install_sub_images() -> None:
     """
     Create sub images and install in them
@@ -51,8 +76,8 @@ def _install_sub_images() -> None:
         qemu.create_sub_image(name)
         file_name = f"{config.QEMU_SUBIMG_NAME}{name}"
         qemu.mount_image(file_name)
-        _chroot_sub_image_install()
-        # qemu.umount_image()
+        _install_sub_image_kernel()
+        qemu.umount_image()
 
 
 def install() -> None:
