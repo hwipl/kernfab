7c8472f1301badff631dc0491b1c038c74a2d826
hwipl <33433250+hwipl@users.noreply.github.com>
2020-07-19 12:46:36 +0200

    remove chroot sub image installing code
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/install.py
+++ kernfab/install.py
@@ -5,41 +5,6 @@ Module for installing the kernel
 from kernfab import config, qemu, run
 
 
-def _chroot_sub_image_install() -> None:
-    """
-    Chroot into sub image directory and install
-    """
-
-    mount_dir = "vm-mount"
-    install_host = ""
-
-    # mount proc, sys and dev into chroot dir
-    cmd = f"mount -t proc /proc {mount_dir}/proc/"
-    run.run_cmd(install_host, cmd)
-    cmd = f"mount --rbind /sys {mount_dir}/sys/"
-    run.run_cmd(install_host, cmd)
-    cmd = f"mount --make-rslave {mount_dir}/sys"
-    run.run_cmd(install_host, cmd)
-    cmd = f"mount --rbind /dev {mount_dir}/dev/"
-    run.run_cmd(install_host, cmd)
-    cmd = f"mount --make-rslave {mount_dir}/dev"
-    run.run_cmd(install_host, cmd)
-
-    # chroot and run install command
-    # add \"su - -c <cmd>\"" to install_cmd?
-    install_cmd = "pwd"     # for testing
-    cmd = f"chroot {mount_dir} /bin/bash -c \"{install_cmd}\""
-    run.run_cmd(install_host, cmd)
-
-    # umount everything in chroot dir
-    cmd = f"umount -R {mount_dir}/proc"
-    run.run_cmd(install_host, cmd)
-    cmd = f"umount -R {mount_dir}/sys"
-    run.run_cmd(install_host, cmd)
-    cmd = f"umount -R {mount_dir}/dev"
-    run.run_cmd(install_host, cmd)
-
-
 def _install_sub_image_kernel() -> None:
     """
     Install kernel in sub image
