5d1c58cd28f586a36f586a632d09019a8af39b3c
hwipl <33433250+hwipl@users.noreply.github.com>
2020-07-22 16:47:42 +0200

    add vm image starting code
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/start.py
+++ kernfab/start.py
@@ -2,8 +2,17 @@
 Module for starting kernel VM(s)
 """
 
+from kernfab import config, qemu
+
 
 def start() -> None:
     """
     Start kernel
     """
+
+    # images = ["1", "2"]
+    images = ["1"]
+    for name in images:
+        vm_image = f"{config.QEMU_SUBIMG_NAME}{name}"
+        vm_id = f"{name}"
+        qemu.run_vm(vm_image, vm_id)
