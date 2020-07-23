9261483016696efd9fd0bf6c9c9c843d90cdf660
hwipl <33433250+hwipl@users.noreply.github.com>
2020-07-23 15:52:57 +0200

    add vm image stopping code
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/stop.py
+++ kernfab/stop.py
@@ -2,8 +2,16 @@
 Module for stopping kernel VM(s)
 """
 
+from kernfab import qemu
+
 
 def stop() -> None:
     """
     Stop kernel VM(s)
     """
+
+    # images = ["1", "2"]
+    images = ["1"]
+    for name in images:
+        vm_id = f"{name}"
+        qemu.stop_vm(vm_id)
