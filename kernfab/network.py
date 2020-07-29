d2cddc5a73980af32c37e8500b0e7df4ab52d244
hwipl <33433250+hwipl@users.noreply.github.com>
2020-07-29 16:29:15 +0200

    add bridge stopping code
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- kernfab/network.py
+++ kernfab/network.py
@@ -32,6 +32,33 @@ def _start_bridge() -> None:
     run.run_cmd(host, ip_cmd)
 
 
+def _stop_bridge() -> None:
+    """
+    Stop vm network bridge
+    """
+
+    host = ""
+    ip_tool = "/usr/bin/ip"
+    bridge_name = "kernfabbr0"
+    bridge_ip = "172.23.32.1/24"
+
+    # remove ip from bridge device
+    ip_cmd = f"{ip_tool} address del {bridge_ip} dev {bridge_name}"
+    run.run_cmd(host, ip_cmd)
+
+    # turn promiscuous mode of on bridge device
+    promisc_cmd = f"{ip_tool} link set {bridge_name} promisc off"
+    run.run_cmd(host, promisc_cmd)
+
+    # set bridge down
+    down_cmd = f"{ip_tool} link set {bridge_name} down"
+    run.run_cmd(host, down_cmd)
+
+    # remove bridge device
+    del_cmd = f"{ip_tool} link del name {bridge_name} type bridge"
+    run.run_cmd(host, del_cmd)
+
+
 def start() -> None:
     """
     Start vm network
@@ -44,3 +71,5 @@ def stop() -> None:
     """
     Stop vm network
     """
+
+    _stop_bridge()
